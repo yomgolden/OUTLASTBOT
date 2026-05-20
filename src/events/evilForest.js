cf3938443cea794637e31b23fc4a5a53ab4c1958
yomgolden <yomadeogun@gmail.com>
2026-05-20 22:08:29 +0100

    Create evilForest.js

---
+++ src/events/evilForest.js
@@ -0,0 +1,27 @@
+module.exports = {
+  name: "Evil Forest",
+
+  intro:
+    "The forest breathes at night. Something is watching.",
+
+  atmosphere: [
+    "🌫 Thick fog swallowed the trees.",
+    "🩸 Blood dripped from broken branches.",
+    "🌧 Rain echoed through the dark woods."
+  ],
+
+  eliminations: [
+    "{victim} disappeared into the darkness.",
+    "{victim} triggered a hidden trap.",
+    "{victim} was dragged into the fog."
+  ],
+
+  survival: [
+    "{player} found abandoned supplies.",
+    "{player} escaped the hunting shadows."
+  ],
+
+  funny: [
+    "{player} ate suspicious berries and survived somehow."
+  ]
+};
