d41c948e029af18bc42db5431b559b418faad4c4
yomgolden <yomadeogun@gmail.com>
2026-05-21 14:34:42 +0100

    Update evilForest.js

--- src/events/evilForest.js
+++ src/events/evilForest.js
@@ -1,6 +1,7 @@
 module.exports = {
-{
   "name": "EVIL FOREST",
+  "difficulty": "hard",
+  
   "introAtmosphere": "Legends spoke of hidden gold buried deep within the forests of the old Benin Kingdom.🌳🪾 Greedy adventurers entered the forest searching for riches before sunrise. By nightfall, the paths behind them had vanished into the fog.🌫️ Strange drums echoed between the trees as shadows moved in silence.🧌 The forest was never protecting treasure. It was feeding on those who searched for it.",
   "openingNarrative": [
     "You followed the old map 🗾 deeper into the forest, even after the villagers begged you to turn back.",
